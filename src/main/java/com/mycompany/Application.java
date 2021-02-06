76912955aa889a980112a744f004bafe6f87b6ac
vamshi <vkreddy1994@gmail.com>
2021-02-06 04:40:40 -0500

    Removed HelloController

--- src/main/java/com/mycompany/Application.java
+++ src/main/java/com/mycompany/Application.java
@@ -1,7 +1,5 @@
 package com.mycompany;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,12 +18,6 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                System.out.println(beanName);
-//            }
 
         };
     }
